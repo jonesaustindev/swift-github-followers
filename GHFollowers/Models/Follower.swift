ae4b74afdb7620c0f2148cb3f046e13a9ac8d21c
jonesaustindev <jonesaustindev@gmail.com>
2021-08-16 19:42:49 -0500

    Add query on bottom of scrollview if follower count greater than 100
    - Images cached on load for collection view

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Austin Jones on 8/10/21.
+//
+
+import Foundation
+
+struct Follower: Codable, Hashable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GHFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,24 @@
+//
+//  UIHelper.swift
+//  GHFollowers
+//
+//  Created by Austin Jones on 8/13/21.
+//
+
+import UIKit
+
+struct UIHelper {
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+}
